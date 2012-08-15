c002a67d20271d4d3331d3963800e695ae962b94
Adam Howard <howard.adam@gmail.com>
2012-08-14 19:41:29 -0500

    lineupo and deps

--- src/lineup/core.clj
+++ src/lineup/core.clj
@@ -1,6 +1,22 @@
 (ns lineup.core
-  (:use clojure.core.logic))
+  (:refer-clojure :exclude [==])
+  (:use [clojure.core.logic]
+        [clojure.tools.macro :as macro]))
 
 (comment
   (run* [q]
         (== q true)))
+
+(comment
+  (run* [q]
+        (appendo [] q [1 2 3 4 5 6 7 8 9 10])))
+
+(comment (run 1 [q]
+              (fresh [vars]
+                     (== q vars)
+                     (everyg #(infd % (domain 1 2 3 4 5 6 7 8 9)) (repeatedly 81 lvar)))))
+
+(defn lineupo [roster ps]
+  (macro/symbol-macrolet [_ (lvar)]
+                         (all
+                          (== [_ _ _ _ _] ps))))
